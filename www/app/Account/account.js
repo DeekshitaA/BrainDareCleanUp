8605bc7439407e09b2a7bfcb87440897011aeae5
DeekshitaA <deekshita.amaravadi@gmail.com>
2016-03-05 13:36:14 -0500

    Account cleanup

---
+++ www/app/Account/account.js
@@ -0,0 +1,50 @@
+(function(){
+
+  //  The Account Settings controller
+angular.module('brainDare')
+  .controller('AccountCtrl', function($scope, $state, Auth, $rootScope, quotes, $cordovaLocalNotification) {
+
+
+    quotes.getQuote().then(function(res) {
+      console.log('res', res);
+      $scope.quote = res;//res.data.contents.quotes[0].quote;
+    });
+
+    this.add = function() {
+
+      var alarmTime = new Date();
+      alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+      var quote = $scope.quote;
+      $cordovaLocalNotification.add({
+        id: "1234",
+        date: alarmTime,
+        message: quote,
+        title: "Task hahaha",
+        autoCancel: true,
+        sound: null
+      }).then(function () {
+        console.log("The notification has been set");
+      });
+    };
+
+    this.isScheduled = function() {
+      $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
+        alert("Notification 1234 Scheduled: " + isScheduled);
+      });
+    };
+
+    this.logout = function (){
+      Auth.$unauth();
+      $state.go('login');
+    };
+
+
+    $rootScope.$on('$cordovaPush:tokenReceived', function(event, data) {
+      alert("Successfully registered token " + data.token);
+      console.log('Ionic Push: Got token ', data.token, data.platform);
+      $scope.token = data.token;
+      quotes.getPushNotification($scope.token);
+    });
+  })
+
+}());
--- www/app/app.js
+++ www/app/app.js
@@ -38,16 +38,12 @@ angular.module("brainDare",
 
   .config(function($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
 
       .state('login', {
         url: '/login',
-        controller: 'loginCtrl',
-        controllerAs: 'login',
+        controller: 'loginCtrl as login',
+     //   controllerAs: 'login',
         templateUrl: 'app/Login/login.html',
       })
 
@@ -81,7 +77,7 @@ angular.module("brainDare",
         views: {
           'dare-account': {
             templateUrl: 'app/Account/account.html',
-         //   controller: 'AccountCtrl as account'
+            controller: 'AccountCtrl as account'
           }
         }
       })
